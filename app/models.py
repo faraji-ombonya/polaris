4113b8d7527dc6c66e54697567603175f1b37b3d
Faraji Ombonya <farajiombonya@gmail.com>
2024-07-01 10:08:40 +0300

    feat: setup book model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from pgvector.django import VectorField
 
 # Create your models here.
+class Book(models.Model):
+    title = models.TextField(null=True, blank=True)
+    author = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    publisher = models.TextField(null=True, blank=True)
+    embeddings = VectorField(
+        dimensions = 100,
+        help_text= "Book embeddings",
+        null = True,
+        blank = True,
+    )
+    
