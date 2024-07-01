c700258d3c14718a2573a1699429bd899ddd97d4
Faraji Ombonya <farajiombonya@gmail.com>
2024-07-01 09:50:36 +0300

    feat: database setup

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'PROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polaris',  
+        'USER': 'polaris',  
+        'PASSWORD': 'polaris', 
+        'HOST': 'localhost',  
+        'PORT': '5432', 
     }
 }
 
