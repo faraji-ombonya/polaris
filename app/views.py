b570a1905b301f29bdaa5eccef490b6ff0f01c1d
Faraji Ombonya <farajiombonya@gmail.com>
2024-07-01 10:21:16 +0300

    feat: setup API skeleton

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from app.models import Book
+from app.serializer import BookSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class BookListView(APIView):
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
