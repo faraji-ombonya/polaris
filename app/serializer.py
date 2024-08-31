b2ba691f9710bb19de25ec80549fcfd438a98cc2
Faraji Ombonya <farajiombonya@gmail.com>
2024-08-31 16:13:47 +0300

    feat: include distance in serializer

--- app/serializer.py
+++ app/serializer.py
@@ -5,4 +5,14 @@ from app.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["id", "title", "author", "description", "category","publisher"]
+        fields = ["id", "title", "author", "description", "category", "publisher"]
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        try:
+            ret["distance"] = instance.distance
+        except:
+            pass
+
+        return ret
